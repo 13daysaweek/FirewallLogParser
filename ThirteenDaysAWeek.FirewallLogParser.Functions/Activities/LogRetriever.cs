27b72d01893c493d6bcd9507f662227a79997c68
Christopher House <chris.house@ecolab.com>
2019-02-18 15:16:25 -0600

    Updated logging

---
+++ ThirteenDaysAWeek.FirewallLogParser.Functions/Activities/LogRetriever.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace ThirteenDaysAWeek.FirewallLogParser.Functions.Activities
+{
+    public class LogRetriever
+    {
+        [FunctionName("GetLogs")]
+        public static async Task GetLogs([ActivityTrigger] DurableActivityContext context, ILogger log)
+        {
+            var hostKey = Convert.FromBase64String(Environment.GetEnvironmentVariable("router-key"));
+            var loginName = Environment.GetEnvironmentVariable("router-username");
+            var hostName = Environment.GetEnvironmentVariable("router-hostname");
+
+            log.LogInformation("Got some variables");
+        }
+    }
+}
